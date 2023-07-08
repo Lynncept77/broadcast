54b97e538ee65a06025ad72b6a8d77674b00b38f
Lynncept <95460649+Lynncept77@users.noreply.github.com>
2023-07-08 21:35:04 +0530

    Add files via upload

---
+++ autocast.py
@@ -0,0 +1,30 @@
+import tgcrypto, time, asyncio
+from pyrogram import Client, idle
+from pyrogram.enums import ChatType
+
+API_ID = 27472480
+API_HASH = "7235e9fd48210c3aa75d4463e6313522"
+SESSION = "BQGjMmAAPFmT4fHGgBhWUva_ExExURjTeT1iyBMDvm7yTnDUBwVTSR9StTM2QaCDZI-sgb_6FnjaISmBe-j2t14LCr28noFKi__ZF08vXAwiBD6ojVfAREySr2F02q0xnO0-R5xYgjRRoycaoIUKbVQbunKdStI2ZQyrQcotEx-MggA8FwxIvE5Zp_xcrr4PGBXCVb6o7gbRWeGUzIM-yfDkhlavK9sMMi2M97rPPDjCkl5FNSxz-l6qVHjKJ1R_L00YmcXPZC2Eb0_ZkzwVAjZ2IPsWAN4PkV1TTg1LFsIotzlXrDX8gMZMAMoUYfXUo3y-AroZkaRuBePrXZehtA29DYfRAwAAAAF0TuZVAA"
+
+post = """
+hello
+"""
+
+client = Client('auto-cast', api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
+client.start()
+
+ while True:
+   try:
+      print("starting broadcast....")
+      for dialog in client.get_dialogs():
+         if dialog.chat.type == ChatType.GROUP or dialog.chat.type == ChatType.SUPERGROUP:
+            try:
+               client.send_message(dialog.chat.id, post)
+               print(f"send in {dialog.chat.title}")
+               time.sleep(10)
+            except Exception as eor:
+               print(eor)
+      print("wait for 1hr")
+      time.sleep(3600)
+   except KeyboardInterrupt:
+      client.stop()
